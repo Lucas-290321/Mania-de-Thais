6f33ff7ac39bc21cd341f5555cde5a9b9c9b5a97
Lucas-290321 <lucaraymundopinheiro@gmail.com>
2026-05-23 02:51:32 -0300

    Atluação

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import link from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion';
 
 function AboutSection() {
@@ -67,12 +67,12 @@ function AboutSection() {
               transition={{ duration: 0.6, delay: 0.3 }}
               className="pt-4"
             >
-              <link
-                to="/sobre"
+              <Link
+                to="/Sobre"
                 className="inline-block px-10 py-4 border-2 border-[hsl(var(--golden-orange))] text-[hsl(var(--golden-orange))] font-bold text-lg rounded-lg transition-all duration-300 hover:bg-[hsl(var(--golden-orange)/0.1)] hover:shadow-[0_0_40px_hsl(var(--golden-orange)/0.4)] active:scale-95"
               >
                 CONHEÇA MINHA HISTÓRIA
-              </link>
+              </Link>
             </motion.div>
           </motion.div>
         </div>
