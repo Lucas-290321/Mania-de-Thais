e57d4bb88f7cfd5e5c6dc2937f76af01183a8143
Lucas-290321 <lucaraymundopinheiro@gmail.com>
2026-05-23 00:02:55 -0300

    Atualização

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -55,7 +55,7 @@ function HeroSection() {
                style={{
                   fontFamily: '"Cormorant Garamond", serif',
                   fontweight: 500,
-                  letterSpacing: '-0.05em',
+                  letterSpacing: '-0.10em',
                   textWrap: 'balance'}}
             >
               Você não é só o seu signo.
