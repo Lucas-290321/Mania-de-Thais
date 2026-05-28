af57ef94f609a5850805737be8d5db00380b0ab3
Lucas-290321 <lucaraymundopinheiro@gmail.com>
2026-05-27 23:03:19 -0300

    Atluação

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,7 +51,7 @@ function Header() {
             <img
               src="/imagem_03.png"
               alt="Logo Thais Lima"
-              className='w-15 h-15 object-contain'
+              className='w-12 h-12 object-contain'
             />  
 
             <div className="leading-none">
